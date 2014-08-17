b138290b959f81a61c1bfbe6fa1b472f40541c04
Cody Sehl <cody.sehl@gmail.com>
2014-08-17 07:31:13 -0400

    adding deploy script

--- travis/build.sh
+++ travis/build.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env sh
 
-echo 'Installing bower packages\n'
+echo '### Installing bower packages ###'
 bower install
 
-echo 'Moving bower packages to their final destinations\n'
+echo '### Moving bower packages to their final destinations ###'
 gulp bower-move
 
-echo 'Building the public directory \n'
+echo '### Building the public directory ###'
 brunch build -P
---
+++ travis/deploy.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env sh
+# Deploys to gh-pages branch then pushes
+
+# Set the git configs so that we know Travis is building this commit
+git config user.email travis
+
+git checkout gh-pages
+# Stage the public dir and stash it
+git add ./public
+git stash
+# Delete everything except .git
+rm -rf ./*
+# Pop the public dir back into our working dir and move it's contents up into ./
+git stash pop
+mv ./public/* .
+rm -r ./public
+# Commit the static files that were in ./public, now at ./
+git add -A
+git commit --allow-empty-message -am ''
+# PUSH IT
+git push
