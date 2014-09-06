f5457b7dc506074adb0af78af53a09795f31d1f1
Cody Sehl <cody.sehl@gmail.com>
2014-09-05 20:21:44 -0500

    cleaning up scaffolding

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp');
 
+var config = {
+	octiconsDest: "./app/assets/img/octicons",
+	stylusDest: "./app/styl/csScaffold"
+}
+
 gulp.task('bower-move', function() {
 	gulp.src('./bower_components/octicons/**')
-		.pipe(gulp.dest('./app/assets/img/octicons'));
+		.pipe(gulp.dest(config.octiconsDest));
+
+	gulp.src('./bower_components/codysehl.net-scaffold/styl/**')
+		.pipe(gulp.dest(config.stylusDest));
 });
 
 gulp.task('default', ['bower-move']);
