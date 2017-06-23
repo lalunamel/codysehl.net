8d17aef72170ad4e5a88a99001a35a1124dfe49c
Cody Sehl <cody.sehl@gmail.com>
2017-06-23 05:50:30 -0600

    Remove octicons

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,10 @@
 var gulp = require('gulp');
 
 var config = {
-	octiconsDest: "./app/assets/img/octicons",
 	stylusDest: "./app/styl/csScaffold"
 }
 
 gulp.task('bower-move', function() {
-	gulp.src('./bower_components/octicons/**')
-		.pipe(gulp.dest(config.octiconsDest));
-
 	gulp.src('./bower_components/codysehl.net-scaffold/styl/**')
 		.pipe(gulp.dest(config.stylusDest));
 });
