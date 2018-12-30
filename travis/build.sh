ae7a7cd29ca336de5b7f203c38481e7268f716aa
Cody Sehl <cody.sehl@gmail.com>
2018-12-30 13:12:10 -0700

    Build improvements

--- travis/build.sh
+++ travis/build.sh
@@ -3,10 +3,10 @@
 # Builds junk
 
 echo '### Installing bower packages ###'
-bower install
+npx bower install
 
 echo '### Moving bower packages to their final destinations ###'
-gulp bower-move
+npx gulp bower-move
 
 echo '### Building the public directory ###'
-brunch build -P 
+npx brunch build -P 
